dd4a61102d68cbc229e535fa61969f53e0f43ec2
Noorsaba05 <156428294+Noorsaba05@users.noreply.github.com>
2025-10-14 14:56:38 +0530

    Add files via upload

---
+++ Assignment3.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+class Complex {
+public:
+    int *r, *i;
+    
+    Complex(int real = 0, int imag = 0){
+        r = new int(real);
+        i = new int(imag);
+    }
+     Complex(const Complex& other) {
+        r = new int(*other.r);
+        i = new int(*other.i);
+    }
+    
+    ~Complex(){
+        delete r;
+        delete i;
+    }
+    
+  
+    
+    void getdata() {
+        int real, imag;
+        cout << "Enter Real and Imaginary Part: ";
+        cin >> real >> imag;
+        *r = real;
+        *i = imag;
+    }
+
+    void display() {
+        if (*i > 0) {
+            cout << *r << " + " << *i << "i" << endl;
+        } else {
+            cout << *r << *i << "i" << endl;
+        }
+    }
+    
+    Complex operator + (Complex c) {
+        return Complex(*r + *c.r, *i + *c.i);
+    }
+       
+    Complex operator - (Complex c){
+        return Complex(*r - *c.r, *i - *c.i);
+    }
+};
+
+int main() {
+    Complex a, b, sum, subs;
+
+    a.getdata();
+    b.getdata();
+
+    sum = a + b;
+    subs = a - b;
+
+    cout << "First Complex Number: ";
+    a.display();
+
+    cout << "Second Complex Number: ";
+    b.display();
+
+    cout << "Addition of Complex Numbers: ";
+    sum.display();
+    
+    cout << "Subtraction of Complex Numbers: ";
+    subs.display();
+    
+    return 0;
+}
+
